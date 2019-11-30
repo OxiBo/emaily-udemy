591b52a481ee04d758af6dcbb449f46b433327c5
OxiBo <xenos2000@ukr.net>
2019-11-30 15:57:29 -0500

    modify keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,13 @@
 // key.js figure out what set of credentials to use
 
-module.exports = {
-  googleClientID:
-    "249710655207-mff59kibdpmdbi1dqsbk43adnr8e9spo.apps.googleusercontent.com",
-  googleClientSecret: "-EFLHc74_OEBuOp4io8X6XZv",
-  mongoURI:
-    "mongodb+srv://OxiBo:jrcfyf1981@cluster0-xsu9k.mongodb.net/test?retryWrites=true&w=majority",
-  cookieKey: "anystring"
-};
+// module.exports = {
+//   googleClientID:
+//     "249710655207-mff59kibdpmdbi1dqsbk43adnr8e9spo.apps.googleusercontent.com",
+//   googleClientSecret: "-EFLHc74_OEBuOp4io8X6XZv",
+//   mongoURI:
+//     "mongodb+srv://OxiBo:jrcfyf1981@cluster0-xsu9k.mongodb.net/test?retryWrites=true&w=majority",
+//   cookieKey: "anystring"
+// };
 
 // cyPkbHuAAgyyvM36    --- 3ebsfFGDsjPDgQcK
 // mongodb+srv://OxiBo:<password>@cluster0-gm3ha.mongodb.net/test?retryWrites=true&w=majority
